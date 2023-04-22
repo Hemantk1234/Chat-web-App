71194f62218ea206ad4a890fdeccb67825640fb8
Hemant Kumbhalkar <hemantkumbhalkar5@gmail.com>
2023-04-22 10:39:54 +0530

    Completed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import { ChatEngine } from "react-chat-engine";
+import LoginForm from "./components/LoginForm";
 import ChatFeed from "./components/ChatFeed";
 import './App.css';
 
 const App = () => {
+    if (!localStorage.getItem('username')) return <LoginForm />;
+
     return (
         <ChatEngine
             height="100vh"
             projectID="a119b959-0707-41d4-a32a-6555a97da246"
-            userName="Hemant"
-            userSecret="442301"
+            userName= {localStorage.getItem('username')}
+            userSecret= {localStorage.getItem('password')}
             renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
         />
     );
